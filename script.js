9ba8c9b5bb273c04c3e5fda07d60efb71e363a41
Wendell Barboza <wendell.junior0020015735@edu.pe.senac.br>
2026-03-23 16:46:11 -0300

    Atualizações

--- script.js
+++ script.js
@@ -3,25 +3,77 @@ async function getPersonagem(){
     let response = await fetch("https://dragonball-api.com/api/characters?limit=58");
     let data = await response.json();
 
-    console.log("Objeto inteiro");
-    console.log(data);
-
-    console.log("Lista de personagens");
     let personagens = data.items;
-    console.log(personagens);
 
-    // sorteando um numero pra escolher um personagem aleatorio
+    // 1º personagem aleatório
     let numeroAleatorio = Math.floor(Math.random() * personagens.length);
-    let personagem = personagens[numeroAleatorio];
+    let p1 = personagens[numeroAleatorio];
 
-    console.log("Personagem sorteado");
-    console.log(personagem);
+    // 2º personagem com KI maior que o primeiro
+    let maisFortes1 = personagens.filter(p =>
+        Number(p.ki) > Number(p1.ki) && p.id !== p1.id
+    );
+
+    let p2;
+
+    if(maisFortes1.length > 0){
+
+        let numeroAleatorio2 = Math.floor(Math.random() * maisFortes1.length);
+        p2 = maisFortes1[numeroAleatorio2];
+
+    }else{
+
+        let outros = personagens.filter(p => p.id !== p1.id);
+        let numeroAleatorio2 = Math.floor(Math.random() * outros.length);
+        p2 = outros[numeroAleatorio2];
+
+    }
+
+
+    // 3º personagem com KI maior que o segundo
+    let maisFortes2 = personagens.filter(p =>
+        Number(p.ki) > Number(p2.ki) && p.id !== p2.id && p.id !== p1.id
+    );
+
+    let p3;
+
+    if(maisFortes2.length > 0){
+
+        let numeroAleatorio3 = Math.floor(Math.random() * maisFortes2.length);
+        p3 = maisFortes2[numeroAleatorio3];
+
+    }else{
+
+        let outros = personagens.filter(p =>
+            p.id !== p1.id && p.id !== p2.id
+        );
+
+        let numeroAleatorio3 = Math.floor(Math.random() * outros.length);
+        p3 = outros[numeroAleatorio3];
+
+    }
+
 
     document.getElementById("personagem").innerHTML = `
-    
-    <h2>${personagem.name}</h2>
-    <img src="${personagem.image}">
-    
-    `
+
+    <div class="card">
+        <h2>${p1.name}</h2>
+        <img src="${p1.image}">
+        <p>KI: ${p1.ki}</p>
+    </div>
+
+    <div class="card destaque">
+        <h2>${p2.name}</h2>
+        <img src="${p2.image}">
+        <p>KI: ${p2.ki}</p>
+    </div>
+
+    <div class="card super">
+        <h2>${p3.name}</h2>
+        <img src="${p3.image}">
+        <p>KI: ${p3.ki}</p>
+    </div>
+
+    `;
 
 }
